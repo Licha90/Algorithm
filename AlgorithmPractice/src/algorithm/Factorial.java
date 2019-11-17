4693bb8d5f6daae80e99e6fe0759c9ddf26683ca
JunMinG <1598595816@qq.com>
2019-11-17 17:34:55 +0000

    This is today's practice

--- AlgorithmPractice/src/algorithm/Factorial.java
+++ AlgorithmPractice/src/algorithm/Factorial.java
@@ -1,27 +1,1 @@
-package algorithm;
-
-import java.math.BigInteger;
-
-public class Factorial {
-
-    public BigInteger factorialCalculation(int number){
-
-        BigInteger result = new BigInteger("1");
-
-        for (int i = 1; i <= number; i++){
-
-            result = result.multiply(new BigInteger(i + ""));// BigInteger(must be some String so "" is needed)
-        }
-
-        return result;
-    }
-
-    public static void main(String[] args) {
-
-        Factorial factorial = new Factorial();
-
-        BigInteger value1 = factorial.factorialCalculation(6);
-
-        System.out.println(value1);
-    }
-}
+package algorithm;import java.math.BigInteger;//this is the factorial calculation codepublic class Factorial {    public BigInteger factorialCalculation(int number){        BigInteger result = new BigInteger("1");        for (int i = 1; i <= number; i++){            result = result.multiply(new BigInteger(i + ""));// BigInteger(must be some String so "" is needed)        }        return result;    }    public static void main(String[] args) {        Factorial factorial = new Factorial();        BigInteger value1 = factorial.factorialCalculation(6);        System.out.println(value1);    }}
